74bcc47c79b25975d162c5886ff7ad0d58301c74
Nathan Cheung <nathancheung77@gmail.com>
2019-07-02 11:40:21 +1000

    Login and Signup

---
+++ app/src/main/java/com/example/teamoracle/LoginActivity.java
@@ -0,0 +1,87 @@
+package com.example.teamoracle;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private TextView password;
+    private TextView username;
+    private Button login;
+    private Button register;
+    private CheckBox rememberMe;
+    private SharedPreferences preferences;
+    private static final String PREFS_NAME = "PrefsFile";
+    private TextView link_forgot_password ;
+    private FirebaseAuth authentication;
+    private FirebaseAuth.AuthStateListener authListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        FirebaseApp.initializeApp(LogInActivity.this);
+        authentication = FirebaseAuth.getInstance();
+        preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        password = findViewById(R.id.passwordText);
+        username = findViewById(R.id.usernameText);
+        login = findViewById(R.id.loginButton);
+        register = findViewById(R.id.createAccountButton);
+        rememberMe = findViewById(R.id.rememberMeCheckbox);
+        link_forgot_password = findViewById(R.id.link_forgot_password);
+
+        link_forgot_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LogInActivity.this, ForgotPasswordActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    public void btn_login_Click (View view){
+        final ProgressDialog progressDialog= ProgressDialog.show(LogInActivity.this, "Please wait", "Processing...", true);
+        (authentication.signInWithEmailAndPassword(username.getText().toString(), password.getText().toString()))
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+                            Intent login = new Intent(LogInActivity.this, DashboardActivity.class);
+                            login.putExtra("Email", authentication.getCurrentUser().getEmail());
+                            login.putExtra("Name", authentication.getCurrentUser().getDisplayName());
+                            startActivity(login);
+                        } else {
+                            Log.e("Wrong email or password", task.getException().toString());
+                            Toast.makeText(LogInActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    public void btn_signUp_Click (View v){
+        Intent signUp = new Intent (LogInActivity.this, SignUpActivity.class);
+        startActivity(signUp);
+    }
+
+}
