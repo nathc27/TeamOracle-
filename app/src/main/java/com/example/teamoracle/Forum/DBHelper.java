e0f56099c53789bbfddfbff1da76e1b69972923a
Apollyn <celineliu1010@gmail.com>
2019-07-31 22:07:36 +1000

    before test

--- app/src/main/java/com/example/teamoracle/Forum/DBHelper.java
+++ app/src/main/java/com/example/teamoracle/Forum/DBHelper.java
@@ -1,7 +1,9 @@
 package com.example.teamoracle.Forum;
 
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -18,18 +20,6 @@ public class DBHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "teamoracle.database.post";
 
-    // Table Creation Statement
-    private static final String CREATE_TABLE_POST = "CREATE TABLE IF NOT EXISTS "
-            + DatabaseContract.QuestionEntry.TABLE_QUESTION
-            + "(" + DatabaseContract.QuestionEntry.KEY_QUESTION_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-            + DatabaseContract.QuestionEntry.QUESTION_CONTENT + " TEXT,"
-            + DatabaseContract.QuestionEntry.QUESTION_CATEGORY + " INTEGER,"
-            + DatabaseContract.QuestionEntry.QUESTION_DIFFICULTY + " INTEGER,"
-            + DatabaseContract.QuestionEntry.QUESTION_ANSWER_OPTIONS_1 + " TEXT,"
-            + DatabaseContract.QuestionEntry.QUESTION_ANSWER_OPTIONS_2 + " TEXT,"
-            + DatabaseContract.QuestionEntry.QUESTION_ANSWER_OPTIONS_3 + " TEXT,"
-            + DatabaseContract.QuestionEntry.QUESTION_ANSWER_OPTIONS_4 + " TEXT,"
-            + DatabaseContract.QuestionEntry.QUESTION_TRUE_ANSWER + " INTEGER" + ")";
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -38,8 +28,12 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         // creating required tables
-        db.execSQL(CREATE_TABLE_QUESTION);
-        db.execSQL(CREATE_TABLE_RECORD);
+        db.execSQL("CREATE TABLE IF NOT EXISTS post ("
+                + "seq INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + "root INTEGER,"
+                + "title TEXT,"
+                + "content TEXT,"
+                + "author TEXT)");
         Log.i("DATABASE SETTING", "TABLE CREATION COMPLETED");
     }
 
@@ -47,36 +41,65 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // on upgrade drop older tables
-        db.execSQL("DROP TABLE IF EXISTS " + DatabaseContract.QuestionEntry.TABLE_QUESTION);
-        db.execSQL("DROP TABLE IF EXISTS " + DatabaseContract.RecordEntry.TABLE_RECORD);
+        db.execSQL("DROP TABLE IF EXISTS post");
         // create new tables
         onCreate(db);
     }
 
     public void cleanDatabase() {
         // on upgrade drop older tables
-        Log.d("CLEAN", "cleaning database");
+        Log.i("CLEAN", "cleaning database");
         SQLiteDatabase db = this.getWritableDatabase();
-        db.execSQL("DROP TABLE IF EXISTS " + DatabaseContract.QuestionEntry.TABLE_QUESTION);
-        db.execSQL("DROP TABLE IF EXISTS " + DatabaseContract.RecordEntry.TABLE_RECORD);
+        db.execSQL("DROP TABLE IF EXISTS post");
         onCreate(db);
-        Log.d("CLEAN", "DATABASE CLEANED");
+        Log.i("CLEAN", "DATABASE CLEANED");
     }
 
-    //TODO: create post query
-    public static void createPost(Post post) {
-
+    //create post
+    public void createPost(Post post) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues c = new ContentValues();
+        c.put("root", post.getRoot());
+        c.put("title", post.getTitle());
+        c.put("content", post.getPostContent());
+        c.put("author", post.getAuthor());
+        db.insert("post", null, c);
+        Log.i("DATABASE", "POST CREATED");
+        db.close();
     }
 
-    //TODO: get root post
-    public static List<Post> getRootPosts() {
+    //get root post
+    public List<Post> getRootPosts() {
         List<Post> rootPosts = new ArrayList<Post>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.rawQuery("SELECT * FROM post WHERE root =" + Post.ROOT_POST + " ORDER BY  seq DESC", null);
+        while (c.moveToNext()) {
+            Post p = new Post();
+            p.setSeq(c.getInt(c.getColumnIndex("seq")));
+            p.setRoot(c.getInt(c.getColumnIndex("root")));
+            p.setTitle(c.getString(c.getColumnIndex("title")));
+            p.setPostContent(c.getString(c.getColumnIndex("content")));
+            p.setAuthor(c.getString(c.getColumnIndex("author")));
+            rootPosts.add(p);
+        }
+        db.close();
         return rootPosts;
     }
 
-    //TODO: get responses for one post
-    public static List<Post> getResponses(int root) {
-        List<Post> rootPosts = new ArrayList<Post>();
-        return rootPosts;
+    //get responses for one post
+    public List<Post> getResponses(int root) {
+        List<Post> responses = new ArrayList<Post>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.rawQuery("SELECT * FROM post WHERE root =" + root + " ORDER BY seq", null);
+        while (c.moveToNext()) {
+            Post p = new Post();
+            p.setSeq(c.getInt(c.getColumnIndex("seq")));
+            p.setRoot(c.getInt(c.getColumnIndex("root")));
+            p.setTitle(c.getString(c.getColumnIndex("title")));
+            p.setPostContent(c.getString(c.getColumnIndex("content")));
+            p.setAuthor(c.getString(c.getColumnIndex("author")));
+            responses.add(p);
+        }
+        return responses;
     }
 }
